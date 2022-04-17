b4d0bd9b024aabd32aa11e8882b141d2c1fb049e
saretotracy <tracysareto@gmail.com>
2022-04-18 00:00:06 +0300

    add create hood view function

--- hood/forms.py
+++ hood/forms.py
@@ -1,8 +1,13 @@
 from django.contrib.auth.models import User
 from django import forms
-from .models import Profile
+from .models import Profile,NeighbourHood
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields=['profile_picture','bio','email','phone_no'] 
+class HoodForm(forms.ModelForm):
+    class Meta:
+        model = NeighbourHood
+        fields=['name','location','description','image']         
+     
--- hood/views.py
+++ hood/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Profile
+from .models import Profile,NeighbourHood
 from django.contrib.auth.models import User
 from .forms import ProfileForm
 from django.contrib.auth.decorators import login_required
@@ -7,8 +7,10 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def index(request):
-      
-      return render(request, 'index.html')
+    current_user = request.user
+    profiles = Profile.objects.filter(id = current_user.id).all()
+    hoods = NeighbourHood.objects.all().order_by('-post_date')    
+    return render(request, 'index.html',{"profiles": profiles, "hoods":hoods})
       
 def update_profile(request):
     current_user = request.user
@@ -29,7 +31,18 @@ def profile(request,pk):
     current_user = request.user
     return render(request,'profile.html',{"current_user":current_user, "user":user, "profiles":profiles})
  
-
+def createhood(request):
+    current_user = request.user
+    form = HoodForm(request.POST, request.FILES)
+    if request.method == 'POST':  
+        if form.is_valid():
+            hood = form.save(commit=False)
+            hood.user = request.user
+            hood.save()
+            return redirect ('index')
+        else:
+            form = ProfileForm()
+    return render(request,'create-hood.html',{'form':form})
 
  
  
