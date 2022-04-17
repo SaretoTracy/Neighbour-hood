6d46aec555892ded975d866187472010408e669e
saretotracy <tracysareto@gmail.com>
2022-04-17 16:34:46 +0300

    add database models

--- neighbour/settings.py
+++ neighbour/settings.py
@@ -130,3 +130,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ 
+REGISTER_REDIRECT_URL='index'
+LOGIN_REDIRECT_URL='index'
+ 
+
