0473b1c91a38e7a3b871c5f2ad5d68f8818395e5
saretotracy <tracysareto@gmail.com>
2022-04-19 05:27:44 +0300

    perform profile test

--- hood/tests.py
+++ hood/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Neighbourhood,Profile
 
 # Create your tests here.
+class ProfileTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.Prof= Profile( id = '1', user ='James',bio='Happy', profile_picture = 'example.jpg',email='chacha@gmail.com',phone_number='0724580020',neighbourhood='Membley' )
+        
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Prof,Profile)) 
