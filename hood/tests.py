4ed0e095cb2b108e6c22409136f8e5ebab6f4f32
saretotracy <tracysareto@gmail.com>
2022-04-19 05:33:15 +0300

    perform  tests

--- hood/tests.py
+++ hood/tests.py
@@ -1,11 +1,11 @@
 from django.test import TestCase
-from .models import Neighbourhood,Profile
+from .models import Neighbourhood,Profile,Business
 
 # Create your tests here.
 class ProfileTestClass(TestCase):
     # Set up method
     def setUp(self):
-        self.Prof= Profile( id = '1', user ='James',bio='Happy', profile_picture = 'example.jpg',email='chacha@gmail.com',phone_number='0724580020',neighbourhood='Membley' )
+        self.Prof= Profile( id = '1', user ='sareto',bio='Wealthy', profile_picture = 'example.jpg',email='sareto@gmail.com',phone_number='0712345678',neighbourhood='Kitengela' )
         
     # Testing  instance
     def test_instance(self):
@@ -15,7 +15,7 @@ class NeighbourhoodTestClass(TestCase):
     # Set up method
     def setUp(self):
         self.Juja = Location.objects.create(name="Juja")
-        self.test_neighbourhood = Neighbourhood.objects.create(name='Membley',location='Nairobi',admin='Chacha',image='example.jpg',description='Beautiful place',occupants='1')
+        self.test_neighbourhood = Neighbourhood.objects.create(name='kitengela',location='Nairobi',admin='sareto',image='example.jpg',description='serene place',occupants='1')
         self.test_neighbourhood.save()
 
 
@@ -35,4 +35,26 @@ class NeighbourhoodTestClass(TestCase):
         
         
 
-  
+           
+class BusinessTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.Busi = Location.objects.create(name="bst)
+
+        self.test_business = Business.objects.create(name='Spashop',image='example.jpg',user='sareto',email='example@gmail.com',phone_number='0712345678',neighbourhood='kitengela')
+
+        self.test_business.save()
+
+    def test_save_method(self):
+        self.test_business.save()
+        test_business = Business.objects.all()
+        self.assertTrue(len(test_business) > 0)
+            
+    
+    def test_delete_method(self):
+        self.Business.delete_business()
+        business = Business.objects.all()
+        self.assertTrue(len(business)==0)   
+        
+    def tearDown(self):
+        Business.objects.all().delete()                                             
