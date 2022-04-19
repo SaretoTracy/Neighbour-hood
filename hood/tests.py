d752e75acae528beda1ffba936bc9f8e7b5cee88
saretotracy <tracysareto@gmail.com>
2022-04-19 05:28:20 +0300

    perform neighbourhood test

--- hood/tests.py
+++ hood/tests.py
@@ -10,3 +10,29 @@ class ProfileTestClass(TestCase):
     # Testing  instance
     def test_instance(self):
         self.assertTrue(isinstance(self.Prof,Profile)) 
+
+class NeighbourhoodTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.Juja = Location.objects.create(name="Juja")
+        self.test_neighbourhood = Neighbourhood.objects.create(name='Membley',location='Nairobi',admin='Chacha',image='example.jpg',description='Beautiful place',occupants='1')
+        self.test_neighbourhood.save()
+
+
+    def test_save_method(self):
+        self.test_neighbourhood.save()
+        test_neighbourhoods = Neighbourhood.objects.all()
+        self.assertTrue(len(test_neighbourhoods) > 0)
+            
+    
+    def test_delete_method(self):
+        self.Neighbourhood.delete_neighbourhood()
+        neighbourhoods = Neighbourhood.objects.all()
+        self.assertTrue(len(neighbourhoods)==0)   
+        
+    def tearDown(self):
+        Neighbourhood.objects.all().delete()
+        
+        
+
+  
